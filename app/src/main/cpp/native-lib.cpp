ee996ac9c50e3e5c32c648527fc84685236f547c
Alex Vainshtein <alexv@anyvision.co>
2020-01-28 15:19:01 +0200

    New approach

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,9 +4,16 @@
 #include "astra_core/astra_core.hpp"
 #include "astra_core/capi/astra_host_events.h"
 #include <android/log.h>
+#include "opencv2/core.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #define TAG "CCode"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,    TAG, __VA_ARGS__)
+int a=0;
+bool isRunning = true;
+
+static const int32_t m_depthMaxValue = (256 * 24); // Depth max value
 
 enum ColorMode
 {
@@ -36,6 +43,44 @@ public:
     }
 };
 
+class DepthFrameListener : public astra::FrameListener {
+public:
+    DepthFrameListener() {
+    }
+
+    virtual void on_frame_ready(astra::StreamReader &reader, astra::Frame &frame) override {
+        LOGD("Frame ready");
+        const astra::DepthFrame depthFrame = frame.get<astra::DepthFrame>();
+
+        int width = depthFrame.width();
+        int height = depthFrame.height();
+        //LOGD("byte_length: %d", depthFrame.byte_length());
+        //LOGD("bytes_per_pixel: %d", depthFrame.bytes_per_pixel());
+        //LOGD("length: %d", depthFrame.length());
+        //LOGD("Image size: %d", image.size);
+        //LOGD("Image rows: %d", image.rows);
+        //image.convertTo(image, CV_8U, 256.0 / m_depthMaxValue);
+        if (a % 100 == 0) {
+            cv::Mat image(height, width, CV_16UC1, (void *) depthFrame.data());
+            image.convertTo(image, CV_8U, 256.0 / m_depthMaxValue);
+            std::vector<int> compression_params;
+            compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+            compression_params.push_back(0);
+            cv::imwrite("/storage/self/primary/Download/" + std::to_string(a) + ".png", image,
+                        compression_params);
+        }
+        a++;
+        /*cv::Mat image(height, width, CV_16UC1, (void*)depthFrame.data());
+        std::vector<int> compression_params;
+        compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+        compression_params.push_back(0);
+        cv::imwrite("/storage/self/primary/Download/"+std::to_string(a)+".png", compression_params);
+        a++;*/
+
+    }
+};
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myapplication_MainActivity_stringFromJNI(
         JNIEnv *env,
@@ -46,30 +91,36 @@ Java_com_example_myapplication_MainActivity_stringFromJNI(
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_myapplication_MainActivity_setUsbNames(
+Java_com_example_myapplication_MainActivity_openDepthStream(
         JNIEnv *env,
-        jobject /* this */, jstring rgbUsbName, jstring depthUsbName) {
-    const char* rgbName = env->GetStringUTFChars(rgbUsbName, 0);
-    const char* depthName = env->GetStringUTFChars(depthUsbName, 0);
-    LOGD("RGB: %s", rgbName);
-    LOGD("Depth: %s", depthName);
-    LOGD("Creating astra");
-    astra_notify_resource_available(rgbName);
-    astra::initialize();
+        jobject /* this */) {
+    isRunning = true;
     LOGD("Astra initialized");
     astra::StreamSet streamSet;
-    astra::StreamReader reader = streamSet.create_reader();
+    astra::StreamReader reader= streamSet.create_reader();
+    auto depthStream = reader.stream<astra::DepthStream>();
+    astra::ImageStreamMode depthMode;
+    depthMode.set_width(640);
+    depthMode.set_height(400);
+    depthMode.set_pixel_format(astra_pixel_formats::ASTRA_PIXEL_FORMAT_DEPTH_MM);
+    depthMode.set_fps(30);
+    depthStream.set_mode(depthMode);
+    depthStream.start();
 
-    reader.stream<astra::ColorStream>().start();
-
-    ColorFrameListener listener;
+    DepthFrameListener listener;
     reader.add_listener(listener);
 
-    astra_update();
-
-
-    if (reader.has_new_frame()) {
-        LOGD("Got a Frame");
+    while (isRunning) {
+        astra_update();
     }
+    depthStream.stop();
+    LOGD("Stream stopped");
+}
 
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_myapplication_MainActivity_stopDepthStream(
+        JNIEnv *env,
+        jobject /* this */) {
+    LOGD("Stopping steam");
+    isRunning = false;
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,15 +11,54 @@ import android.util.Log
 import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
+import com.orbbec.astra.*
+import com.orbbec.astra.android.AstraAndroidContext
+import com.orbbec.astra.android.AstraDeviceManagerListener
+import java.lang.Exception
 import java.util.ArrayList
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
-class MainActivity : AppCompatActivity() {
-    var rgbUsbName: String? = null
-    var depthUsbName: String? = null
+class MainActivity : AppCompatActivity() , AstraDeviceManagerListener {
+    var rgbUsbName: String? = "jygjygj"
+    var depthUsbName: String? = "jygjygj"
     lateinit var manager: UsbManager
     val devices = ArrayList<UsbDevice>()
-    private val usbReceiver = object : BroadcastReceiver() {
+    private var aac: AstraAndroidContext? = null
+    private var executor: ExecutorService? = null
+
+    private val runnable = Runnable {
+        try {
+            openDepthStream()
+            /*val streamSet = StreamSet.open()
+            val reader = streamSet.createReader()
+            val depthStream = DepthStream.get(reader)
+            depthStream.mode = ImageStreamMode(0, 640, 400, 100, 30)
+            depthStream.registration = true
+            val frameListener = StreamReader.FrameListener {
+                    streamReader, frame ->
+                val df = DepthFrame.get(frame)
+                Log.i("Test", "Received frame")
+            }
+            reader.addFrameListener (frameListener)
+            depthStream.start()
+
+            while (!exit) {
+                Astra.update()
+                TimeUnit.MILLISECONDS.sleep(20L)
+            }
+            reader.removeFrameListener(frameListener)
+            depthStream.stop()
+            reader.destroy()
+            streamSet.close()*/
+        }
+        catch (e: Exception) {
+
+        }
+    }
+    /*private val usbReceiver = object : BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
             if (ACTION_USB_PERMISSION == intent.action) {
@@ -34,8 +73,14 @@ class MainActivity : AppCompatActivity() {
                             manager.requestPermission(deviceTmp, permissionIntent)
                         }
                         else {
-                            setUsbNames(rgbUsbName!!, depthUsbName!!)
-                            Log.d("Test", "All permissions approved")
+                            val connection = manager.openDevice(device)
+                            if(connection != null) {
+                                setUsbNames(rgbUsbName!!, depthUsbName!!)
+                                Log.d("Test", "All permissions approved")
+                            }
+                            else {
+
+                            }
                         }
                     } else {
                         Log.d("Test", "permission denied for device $device")
@@ -43,27 +88,31 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 12345)
+
         manager = getSystemService(Context.USB_SERVICE) as UsbManager
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
-        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        /*val filter = IntentFilter(ACTION_USB_PERMISSION)
         registerReceiver(usbReceiver, filter)
         val deviceList = manager.deviceList
         deviceList.forEach {
-            if(it.value.vendorId == 11205) {
+            if(it.value.vendorId == 11205 && it.value.productId == 1544) {
                 devices.add(it.value)
-                if(it.value.productName?.contains("Depth") == true) {
+                depthUsbName = it.value.deviceName
+                *//*if(it.value.productName?.contains("Depth") == true) {
                     depthUsbName = it.value.deviceName
+
                 }
                 else {
                     rgbUsbName = it.value.deviceName
-                }
+                }*//*
                 Log.i("Test", "Device id: ${it.value.deviceId}")
                 Log.i("Test", "Device name: ${it.value.deviceName}")
                 Log.i("Test", "Product name: ${it.value.productName}")
@@ -75,7 +124,22 @@ class MainActivity : AppCompatActivity() {
             val device = devices[0]
             devices.removeAt(0)
             manager.requestPermission(device, permissionIntent)
-        }
+        }*/
+
+        aac = AstraAndroidContext(applicationContext, this)
+        aac?.initialize()
+        aac?.openAllDevices()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopDepthStream()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        aac?.terminate()
+        //unregisterReceiver(usbReceiver)
     }
 
     /**
@@ -83,7 +147,8 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     private external fun stringFromJNI(): String
-    private external fun setUsbNames(rgbUsbName: String, depthUsbName: String)
+    private external fun openDepthStream()
+    private external fun stopDepthStream()
 
 
     companion object {
@@ -92,5 +157,23 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+    override fun onOpenAllDevicesCompleted(p0: MutableIterable<UsbDevice>?) {
+        executor = Executors.newSingleThreadExecutor().apply {
+            execute(runnable)
+        }
+    }
+
+    override fun onOpenDeviceCompleted(p0: UsbDevice?, p1: Boolean) {
+
+    }
+
+    override fun onPermissionDenied(p0: UsbDevice?) {
+
+    }
+
+    override fun onNoDevice() {
+
     }
 }
